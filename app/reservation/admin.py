10b63c853434814eb2b7492c9e87b500b4e3f398
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-10-10 12:51:54 +0900

    update admin page

--- app/reservation/admin.py
+++ app/reservation/admin.py
@@ -7,26 +7,34 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import *
 
-# 출력할 ResourceAdmin 클래스를 만든다
+class SeminarInline(admin.StackedInline):
+    model = Reservation
+    extra = 0
+    ordering = ('state',)
+
 class UserAdmin(admin.ModelAdmin):
-  list_display = ('user_id', 'password','name','phone','email','major','student_id')
+    list_display = ('user_id','name','phone','email','major','student_id')
 
 class NoticeAdmin(admin.ModelAdmin):
-  list_display = ('date','title','content')
+    list_display = ('title','user_id','date','content')
 
 class ReservationAdmin(admin.ModelAdmin):
-  list_display = ('reservation_id','user_id','name','date','period','classroom','state','day','purpose','reject')
+    list_display = ('name','user_id','date','period','classroom','state','day','purpose','reject')
+    list_editable = ('state',)
 
 class InquireAdmin(admin.ModelAdmin):
-    list_display = ('inquire_id','date','user_id','title','content')
+    list_display = ('title','user_id','date','content')
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('inquire_view','comment_id','user_id','date','comment')
+    list_display = ('inquire_title','user_id','date','comment')
 
-    def inquire_view(self, obj):
-        return obj.inquire.inquire_id
+    def inquire_title(self, obj):
+        return obj.inquire.title
+    inquire_title.short_description = '문의내용'
 
     raw_id_fields = ('inquire',)
+
+
 
 # 클래스를 어드민 사이트에 등록한다.
 admin.site.register(User, UserAdmin)
